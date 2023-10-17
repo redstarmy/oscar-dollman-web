72b2a2e4404e87cc26190ccae877ca11bde726c3
redstarmy <58826939+redstarmy@users.noreply.github.com>
2023-10-17 10:41:50 +0200

    implemented sliding nav

--- src/components/SlidingNav.vue
+++ src/components/SlidingNav.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
     <nav>
-      <RouterLink to="/" @mouseleave="highlighted = activeTab" @mouseover="highlighted='home'"
-                  v-on:click="activeTab='home'">Home
+      <RouterLink to="/" @mouseleave="offset = active" @mouseover="offset = calcOffset('home')"
+                  v-on:click="active=0">Home
       </RouterLink>
-      <RouterLink to="/gallery" @mouseleave="highlighted = activeTab" @mouseover="highlighted='gallery'"
-                  v-on:click="activeTab='gallery'">Gallery
+      <RouterLink to="/gallery" @mouseleave="offset = active" @mouseover="offset = calcOffset('gallery')"
+                  v-on:click="active=1">Gallery
       </RouterLink>
-      <RouterLink to="/social" @mouseleave="highlighted = activeTab" @mouseover="highlighted='social'"
-                  v-on:click="activeTab='about'">Social
+      <RouterLink to="/social" @mouseleave="offset = active" @mouseover="offset = calcOffset('social')"
+                  v-on:click="active=2">Social
       </RouterLink>
     </nav>
-    <div class="tab_underline"
-         v-bind:style="{marginLeft: 33.33 * Math.max(routes.indexOf(String(highlighted)), 0) + '%'}"></div>
+    <div class="tab_underline" v-if="curRoute.name"
+         v-bind:style="{marginLeft: 33.33 * offset + '%'}"></div>
   </div>
+
 </template>
 
 <style scoped>
@@ -41,11 +42,22 @@ nav a {
 
 <script lang="ts" setup>
 import {RouterLink, useRoute} from "vue-router";
-import {computed, ref} from "vue";
+import {onMounted, ref} from "vue";
+import router from "@/router";
 
 const routes = ["home", "gallery", "social"]
-const route = useRoute()
+const curRoute = useRoute()
 
-const activeTab = ref(computed(() => route.name))
-const highlighted = ref(computed(() => route.name))
+const active = ref(0)
+const offset = ref(0)
+onMounted(async () => {
+  await router.isReady()
+  active.value = calcOffset(curRoute.name)
+  offset.value= calcOffset(curRoute.name)
+})
+
+const calcOffset = (tab) => routes.indexOf(tab)
+
+
+
 </script>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="gallery">
     <a>This is a gallery page</a>
+
   </div>
 </template>
+<script>
+
+export default {
+  data() {
+    return {
+      gap: 20,
+      align: "justify",
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
