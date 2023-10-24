da3055af24f34ab3497c75b8389aae82c7c7ba81
redstarmy <58826939+redstarmy@users.noreply.github.com>
2023-10-24 20:36:52 +0200

    added modules for express and lazy loading

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueLazyload from "vue-lazyload";
 
 const app = createApp(App)
 
-app.use(VueLazyload, {
-  attempt: 3
-})
+
 
 app.use(router)
 
