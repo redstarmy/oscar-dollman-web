6a54c934f209750f446bdbc454177730f55a2fcf
redstarmy <58826939+redstarmy@users.noreply.github.com>
2023-10-31 11:17:45 +0100

    refactored + removed proprietary lazy loaders

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,24 +1,36 @@
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div class="description">
-    <p class="text">This is my website. It serves the purpose of a <RouterLink class="link" to="/gallery">gallery</RouterLink> to showcase my photography work and a platform to introduce myself to the world.</p>
-    <br>
-    <p class="text"> It is built using a <a target="_blank" class="link" href="https://vuejs.org/">Vue.js</a> frontend together with a simple <a target="_blank" class="link" href="https://expressjs.com/">Express</a> backend, mainly to serve images. More on that in my <a target="_blank" class="link" href="https://github.com/redstarmy/oscar-dollman-web">GitHub</a> repository.</p>
-    <br>
+    <p class="text">
+      This is my website. It serves the purpose of a
+      <RouterLink class="link" to="/gallery">gallery</RouterLink> to showcase my photography work
+      and a platform to introduce myself to the world.
+    </p>
+    <br />
+    <p class="text">
+      It is built using a
+      <a target="_blank" class="link" href="https://vuejs.org/">Vue.js</a> frontend together with a
+      simple <a target="_blank" class="link" href="https://expressjs.com/">Express</a> backend,
+      mainly to serve images. More on that in my
+      <a target="_blank" class="link" href="https://github.com/redstarmy/oscar-dollman-web"
+        >GitHub</a
+      >
+      repository.
+    </p>
+    <br />
     <p class="text">Enjoy :)</p>
   </div>
 </template>
 
 <style scoped>
-.description{
+.description {
   padding: 30px 10px;
 }
-.link{
+.link {
   color: black;
 }
-.text{
+.text {
   display: block;
   font-size: 20px;
 }
