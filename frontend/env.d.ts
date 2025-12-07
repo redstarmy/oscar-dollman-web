4ba3f8d41bd15b61173f8427dec6dead58fc334a
redstarmy <58826939+redstarmy@users.noreply.github.com>
2025-12-07 16:54:19 +0100

    avoid vue-tsc 3.1.6 bug + cleaned up

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,3 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module '@/components/LazyPlaceholderImage.vue'
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, any>
+  export default component
+}
